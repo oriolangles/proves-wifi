9f10f887b07a1a4bb6fe95ae826f2465fd7b1496
oriolangles <111228537+oriolangles@users.noreply.github.com>
2022-11-13 12:56:03 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,10 +12,18 @@ function pantalla_amb_dades () {
     OLED.writeStringNewLine("TDR")
     OLED.newLine()
     OLED.writeStringNewLine("Temperatura: " + temperatura + " C")
-    OLED.writeStringNewLine("Humitat: " + Humitat + " %")
+    OLED.writeStringNewLine("Humitat: " + Humitat + "%")
     OLED.writeStringNewLine("Pressio: " + Pressió + " hPa")
     OLED.writeStringNewLine("Intensitat: " + intensitat + " mA")
 }
+function Voltatge_bateria () {
+    batvolt = 0
+    for (let index = 0; index < 10; index++) {
+        batvolt += BME280.temperature(BME280_T.T_C)
+    }
+    batvolt = batvolt / 10
+}
+let batvolt = 0
 let intensitat = 0
 let Pressió = 0
 let Humitat = 0
@@ -33,26 +41,7 @@ if (ESP8266_IoT.wifiState(true)) {
 basic.pause(1000)
 let pantalla = 1
 basic.forever(function () {
-    ESP8266_IoT.connectThingSpeak()
-    if (ESP8266_IoT.thingSpeakState(true)) {
-        lectura_de_dades()
-        ESP8266_IoT.setData(
-        "8OUGW8MHUV093H5B",
-        temperatura,
-        Humitat,
-        Pressió,
-        intensitat
-        )
-        ESP8266_IoT.uploadData()
-    } else {
-        OLED.clear()
-        OLED.writeStringNewLine("TDR")
-        OLED.newLine()
-        OLED.writeStringNewLine("ERROR DADES")
-    }
-    basic.pause(300000)
-})
-basic.forever(function () {
+    lectura_de_dades()
     if (pantalla == 1) {
         pantalla_amb_dades()
         basic.pause(2000)
@@ -62,9 +51,32 @@ basic.forever(function () {
         OLED.writeStringNewLine("TDR")
         OLED.newLine()
         OLED.writeStringNewLine("BATERIA")
+        Voltatge_bateria()
+        OLED.writeStringNewLine("Volt " + batvolt)
         basic.pause(2000)
     }
     if (pantalla > 2) {
         pantalla = 1
     }
+})
+basic.forever(function () {
+    ESP8266_IoT.connectThingSpeak()
+    if (ESP8266_IoT.thingSpeakState(true)) {
+        Voltatge_bateria()
+        ESP8266_IoT.setData(
+        "8OUGW8MHUV093H5B",
+        temperatura,
+        Humitat,
+        Pressió,
+        intensitat,
+        batvolt
+        )
+        ESP8266_IoT.uploadData()
+    } else {
+        OLED.clear()
+        OLED.writeStringNewLine("TDR")
+        OLED.newLine()
+        OLED.writeStringNewLine("ERROR DADES")
+    }
+    basic.pause(300000)
 })
