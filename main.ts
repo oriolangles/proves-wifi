69d6019cf97a58294f55d26c5b328ffa31b6a530
oriolangles <111228537+oriolangles@users.noreply.github.com>
2022-11-15 07:16:46 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,10 +5,11 @@ function PantallaDades () {
     OLED.writeStringNewLine("Temperatura: " + ("" + temperatura) + " C")
     OLED.writeStringNewLine("Humitat: " + ("" + Humitat) + "%")
     OLED.writeStringNewLine("Pressio: " + ("" + Pressió) + " hPa")
-    OLED.writeStringNewLine("Intensitat: " + ("" + intensitat) + " mA")
+    OLED.writeStringNewLine("Bateria" + ("" + PercBat) + "%")
+    OLED.writeStringNewLine("Voltatge:  " + ("" + batvolt) + " V")
 }
 input.onButtonPressed(Button.A, function () {
-    pantalla += 1
+    Sos()
 })
 function LlegirDades () {
     temperatura = BME280.temperature(BME280_T.T_C)
@@ -25,7 +26,8 @@ function EnviarDades () {
         Humitat,
         Pressió,
         PercBat,
-        batvolt
+        batvolt,
+        sos
         )
         ESP8266_IoT.uploadData()
     } else {
@@ -35,16 +37,27 @@ function EnviarDades () {
         OLED.writeStringNewLine("ERROR DADES")
     }
 }
+function Sos () {
+    pantalla = 1
+    basic.pause(15000)
+    sos = 1
+    EnviarDades()
+    sos = 0
+    basic.pause(15000)
+    EnviarDades()
+    basic.pause(2000)
+    pantalla = 0
+}
 function VoltatgeBateria () {
     batvolt = 0
     for (let index = 0; index < 100; index++) {
         batvolt += pins.analogReadPin(AnalogPin.P1)
     }
-    batvolt = batvolt / 100 * 3.187 / 1023 * 110 / 10
+    batvolt = batvolt / 100 * 3.125 / 1023 * 110 / 10
     PercentatgeBat()
 }
 function PercentatgeBat () {
-    if (batvolt == 4.2) {
+    if (batvolt >= 4.2) {
         PercBat = 100
     }
     if (batvolt < 4.2 && batvolt >= 4) {
@@ -63,17 +76,28 @@ function PercentatgeBat () {
         PercBat = 0
     }
 }
+function PantallaSos () {
+    OLED.clear()
+    OLED.newLine()
+    OLED.newLine()
+    OLED.newLine()
+    OLED.writeStringNewLine("SOS")
+    OLED.writeStringNewLine("Enviant dades...")
+}
+let intensitat = 0
 let batvolt = 0
 let PercBat = 0
-let intensitat = 0
 let Pressió = 0
 let Humitat = 0
 let temperatura = 0
+let sos = 0
+let pantalla = 0
 OLED.init(128, 64)
 OLED.writeStringNewLine("TDR")
 OLED.newLine()
+pantalla = 0
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
-ESP8266_IoT.connectWifi("MAZINGERZ", "letsthesunshine")
+ESP8266_IoT.connectWifi("orioloneplusnord", "bondia1234")
 if (ESP8266_IoT.wifiState(true)) {
     OLED.writeStringNewLine("Connectat")
 } else {
@@ -82,26 +106,20 @@ if (ESP8266_IoT.wifiState(true)) {
 LlegirDades()
 VoltatgeBateria()
 basic.pause(1000)
-let pantalla = 1
-basic.forever(function () {
-    EnviarDades()
-    basic.pause(900000)
-})
+pantalla = 0
+sos = 0
 basic.forever(function () {
     LlegirDades()
     VoltatgeBateria()
-    if (pantalla == 1) {
+    if (pantalla == 0) {
         PantallaDades()
         basic.pause(2000)
     }
-    if (pantalla == 2) {
-        OLED.clear()
-        OLED.writeStringNewLine("TDR")
-        OLED.newLine()
-        OLED.writeStringNewLine("BATERIA:" + ("" + PercBat) + "%")
-        basic.pause(2000)
+    if (pantalla == 1) {
+        PantallaSos()
     }
-    if (pantalla > 2) {
-        pantalla = 1
-    }
+})
+basic.forever(function () {
+    EnviarDades()
+    basic.pause(900000)
 })
